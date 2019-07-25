859a7e099175d84b7b2c2e9a5a7dc52766197cf7
Zoran Karic <zoran@karic.uk>
2019-07-25 08:43:20 +0000

    Generic seeder

--- node-api-over-buz/database/seeds/seed_i18n_namespaces_table.js
+++ node-api-over-buz/database/seeds/seed_i18n_namespaces_table.js
@@ -1,22 +1,4 @@
-exports.seed = knex => {
-  // Deletes ALL existing entries
-  return knex('i18n_namespaces')
-    .truncate()
-    .then(() => {
-      // Inserts seed entries
-      return knex('i18n_namespaces').insert([
-        {
-          id: 1,
-          route: 'admin',
-          key: 'buttons',
-          createdAt: knex.fn.now(),
-        },
-        {
-          id: 2,
-          route: 'admin',
-          key: 'home',
-          createdAt: knex.fn.now(),
-        },
-      ]);
-    });
-};
+const insert = require('../data');
+const data = require('../data/i18n_namespaces.json');
+
+exports.seed = knex => insert(knex, 'i18n_namespaces', data);
