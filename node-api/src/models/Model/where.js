e5af140ef68b803b32adfd20e2f3f5413439257e
Zoran Karic <zoran.karic@loanable.com>
2020-03-03 20:04:21 +0000

    With bin: filter

--- node-api/src/models/Model/where.js
+++ node-api/src/models/Model/where.js
@@ -1,10 +1,13 @@
 /* eslint func-names: ["error", "never"] */
 
+const binRegEx = /^bin:\d+$/g;
+
 const isPropValue = (model, key, value) =>
   typeof value !== 'undefined' && typeof value !== 'object' && model.has(key);
 
 const baseWhereQuery = (model, whereParams) => {
   const query = model.baseQuery();
+  const db = model.db();
 
   if (typeof whereParams === 'object') {
     Object.keys(whereParams).forEach(key => {
@@ -25,6 +28,9 @@ const baseWhereQuery = (model, whereParams) => {
           query.whereNull(key);
         } else if (value.toUpperCase && value.toUpperCase() === 'NOT NULL') {
           query.whereNotNull(key);
+        } else if (value.match && value.match(binRegEx)) {
+          const b = parseInt(value.substr(4), 10);
+          query.where(db.raw(`?? & ${b} = ${b}`, [key]));
         } else {
           query.where(key, value);
         }
--- node-api/src/routes/api/komMain.js
+++ node-api/src/routes/api/komMain.js
@@ -2,13 +2,15 @@ const express = require('express');
 const crudController = require('../../controllers/crudController');
 const ctrl = require('../../controllers/api/komMainController');
 const KomMain = require('../../models/KomMain');
+const KomMainView = require('../../models/KomMainView');
 
 const router = express.Router();
 
 const crudCtrl = crudController(KomMain);
+const viewCtrl = crudController(KomMainView);
 router.get('/next', ctrl.nextSifra);
 router.get('/init', crudCtrl.init);
-router.get('/', crudCtrl.allPaginated);
+router.get('/', viewCtrl.allPaginated);
 router.post('/', KomMain.validate(), crudCtrl.create);
 router.get('/:id', crudCtrl.find);
 router.put('/:id', KomMain.validate(), crudCtrl.update);
